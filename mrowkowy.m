4bcc15b9dbcbcee2beb1d68909c97c372e4997c4
halima <halima@172.20.10.6>
2016-11-07 14:49:18 +0100

    losowanie wezla przez mrowki

--- mrowkowy.m
+++ mrowkowy.m
@@ -1,14 +1,13 @@
 clear;
 clc;
-%function [besttour,mincost]=MyAlgorithm(n,iter,M);
-
 
 H=30; %zakres zmian warto¶ci parametrów
 M=15; %liczba mrówek
 n=3; %liczba parametrów
 iter=20; %max liczba iteracji
 alfa=1;
-beta=1;
+Q=0.2;%wspó³czynnik skaluj±cy ilo¶æ zostawianych feromonów
+ro=0.05 %warto¶æ szybko¶ci parowania ¶ladu feromonowego
 
 %Za³o¿one warto¶ci graniczne dla parametrow X1,X2,X3
 X1min=-300e-5;
@@ -45,9 +44,10 @@ kk3=rand;
 k3=(((-100e-5 - (-300e-5)) .* kk3) + (-300e-5)) - mod((((-100e-5 - (-300e-5)) .* kk3) + (-300e-5)) , 0.00000001);  
 K123=[k1 k2 k3]
 
-[blad_koncowy]=funkcjabledu(K123)
+[blad_koncowy]=funkcjabledu(K123) %randomowy wska¼nik jako¶ci
 
-%Tworzenie macierzy feromonów
+%Tworzenie macierzy feromonów na podstawie uzyskanego wy¿ej wska¼nika, dla
+%ka¿dego rozwi±zania macierzy taka sama warto¶æ
 for j=1:n
     for i=1:H
         feromony(i,j)=1/blad_koncowy;
@@ -55,6 +55,7 @@ for j=1:n
     end
 end
 
+%tworzenie macierzy prawdopodobieñstw na podstawie wzoru
     for j=1:n
         for i=1:H
     alfa_feromony=feromony.^alfa;
@@ -64,35 +65,48 @@ end
     end
 
     
-%losowanie randomowej wartosci dla pierwszego parametru z zakresu
-%X1min-X1max
-o=rand(M,1); 
-losowa=(((-100e-5 - (-300e-5)) .* o) + (-300e-5)) - mod((((-100e-5 - (-300e-5)) .* o) + (-300e-5)) , 0.00000001); 
-trasa(:,1)=losowa
-  
-   for i=1:M
-       for j=1:n-1
+    
+     %pêtla wyboru wêz³a
+%     for l=iter
+    for i=1:M
+         for j=1:n
             r=rand;
             s=0;
             for k=1:H %numer wezla
                 s=s+prawdop(k,j);%uzycie kola ruletki
                 if r<=s
-                    trasa(i,j+1)=parametry(k,j+1); %umieszczenie mrowki i w nastepnym wezle
+                    trasa(i,j)=parametry(k,j); %zapisywana warto¶æ wybranego wêz³a w macierzy trasy
                     break
                 end
             end    
        end
-         %wyznaczanie wskanika jako¶ci
-%         rozwiazanie()=trasa(i,:)
-%            [blad_koncowy1(i)]=funkcjabledu(rozwiazanie(i))
-          
+     
+         %wyznaczanie wskaznika jako¶ci dla trasy pierwszej mrówki
+            %[JE]=funkcjabledu(trasa);
+            trasa_mrowki(1,:)=trasa(i,:) %trasa i-tej mrówki(i-ty wiersz macierzy trasa)
+            
+            wskaznik(i,1)=funkcjabledu(trasa_mrowki);
+            
             
             %aktualizacja lokalna feromonów
-            
-    end
-    
-         
-
-    
+%             for j=1:n
+%                 for i=1:H
+%                    
+%                       feromony(i,j)=feromony(i,j)+(Q/(wskazniki))
+%  
+%                 end
+%             end
+     end
+%     %sprawdzanie czy najlepszy wska¼nik
+%     [JE_cyklu]=funkcjabledu(trasa)
+%     if JE_cyklu_nowy<JE_cyklu
+%         trasa
+%         
+%     %aktualizacja globalna feromonów
+%     feromony(i,j)=(1-ro)feromony(i,j)+(1/(JE_cyklu_))
+%     
+%     end
+% 
+%     
 
 
